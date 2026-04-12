f90c4b00b9446601ee183c08fee2a861118e43b2
anneso09 <annesophie.montenot@gmail.com>
2026-04-12 14:27:10 +0400

    test: tests unitaires AuthTest + FormationTest + SecuriteTest + factories User/Formation

--- skillhub-backend/app/Models/Formation.php
+++ skillhub-backend/app/Models/Formation.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Formation extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'titre',
         'description',
